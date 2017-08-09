0dbedcf35bb927c2491634df1aa280752207b867
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-08-08 22:08:48 -0400

    fixing the css for full details. check out the secrets page :godmode:

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -5,6 +5,7 @@
 
         function initiateEntrance() {
           var wedge = $('#wedge');
+          var stage = $('#stage');
           wedge.velocity({
             backgroundPositionX: -200,
             backgroundPositionY: -580
@@ -13,13 +14,23 @@
             duration: 2000
           });
           wedge.velocity({
-            translateX: '+=5px',
             backgroundPositionY: -660
           }, {
             easing: [2],
-            duration: 500,
+            duration: 400,
             loop: 5
-          })
+          });
+          stage.velocity({
+            translateX: [216]// 36 x 5 steps + 36
+          }, {
+            easing: 'easeInOutSine',
+            duration: 4000,
+            delay: 2000
+          });
+          wedge.velocity({
+            backgroundPositionX: 0,
+            backgroundPositionY: -580
+          }, [1])
         }
         initiateEntrance();
     }
