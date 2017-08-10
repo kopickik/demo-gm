d206a2cda7f9e9f22d3fe4f2b8973e105d8665ce
Kyle Kopicki <kyle.kopicki@gmail.com>
2017-08-10 00:44:30 -0400

    new type of logging

--- routes/customers/all.js
+++ routes/customers/all.js
@@ -3,6 +3,7 @@
 const Customer = require('../../models/Customer');
 
 module.exports = (req, res, next) => {
+  req.app.emit('customers:getAll', req, res);
   Customer.find({}, function (err, customers) {
     if (err) return res.status(500).send('There was an issue fetching customers.')
     res.status(200).send(customers)
--- server.js
+++ server.js
@@ -3,16 +3,33 @@
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
-const logger = require('morgan')
+const winston = require('winston')
+const rlog = require('./node_modules/bunyan-request-logger/request-logger.js')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const helmet = require('helmet')
+const request = require('request')
+const noCache = require('connect-cache-control')
+const errorHandler = require('express-error-handler')
+const http = require('http')
 const Customer = require('./models/Customer')
 const dbConfig = require('./config/db')
 const routes = require('./routes')
 
 const app = express()
+var log = rlog()
+var env = process.env
+
+app.use(log.requestLogger())
+
+app.get('/log.gif', noCache, log.route() )
+
+app.get('/error', function createError(req, res, next) {
+  var err = new Error('Sample error')
+  err.status = 500
+  next(err)
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -33,16 +50,37 @@ app.use(function (req, res, next) {
 
 // Base setup (db)
 mongoose.Promise = global.Promise
-mongoose.connect(dbConfig.url)
+mongoose.connect(dbConfig.url).then(function () {
+
+}).catch(function (err) {
+  app.emit('event:db_err', err.message)
+});
 
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// app.on('event:db_connected', function () {
+//   logger('Database connected.' + new Date())
+// })
+// app.on('event:db_err', function (message) {
+//   request('http://localhost:8080/api');
+// })
+// app.on('customers:getAll', function (req, res) {
+//   logger(req)
+//   logger(res)
+// })
+// db.once('connected', function () { app.emit('event:db_connected')})
 
-app.use(express.static(__dirname + '/app'))
-app.use(logger('short'))
+// app.use(express.static(__dirname + '/app'))
+// app.use(logger('short'))
 
 app.use('/api', routes)// all of our API routes
 
-app.listen(port)
-console.log('Express app listening on port ' + port)
+app.use(log.errorLogger())
 
+var server = http.createServer(app);
+
+app.use(errorHandler({server: server}))
+
+server.listen(port, function () {
+  log.info('Listening on port ' + port)
+})
